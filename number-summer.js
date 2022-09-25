06171b0d0ea76e575e7add11a180768ab8364965
WarriorGoat <michael.d.spitz@gmail.com>
2022-09-25 16:38:45 -0400

    Fixed incorrect upload.

--- number-summer.js
+++ number-summer.js
@@ -8,13 +8,26 @@
 */
 
 const prompt = require('prompt-sync')({sigint: true});
+let test = false
 
-console.log("Please enter a number from 1-100 to double.");
-let number = Number(prompt(">>  "));
-//let sum = 0
-while (number <= 100) {
-    number = number *2;
-    console.log(number);
-    
+console.log("This program will add numbers you enter until you enter a zero to stop the program.");
+let sum = Number(prompt("Please enter the first number.  >>  "));
+console.log(`You entered: ${sum}`);
+if (sum >0) {
+    test = true;
 }
-//console.log(`Your total is ${sum}.`)
+let number = 0
+
+while (test === true) {
+    console.log(" ");
+    console.log(`Please enter the next number to add to the total.  You may enter zero to stop the program.`);
+    number = Number(prompt(`>>`));
+    console.log(`You entered: ${number}`);
+    if (number >0) {
+        test = true;
+    }else {test = false};
+    console.log(`The sum of ${sum} and ${number} is:  `);
+    sum = number + sum;
+    console.log(sum);
+    console.log(" ");
+}
